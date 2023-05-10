646e5dfcbff6eec24abb9d4c28084622d9443634
himangshumahato82 <himangshumahato82@gmail.com>
2023-05-10 16:16:25 +0530

    update

--- taiyo/src/Components/Contacts/Contact.jsx
+++ taiyo/src/Components/Contacts/Contact.jsx
@@ -1,12 +1,68 @@
 import React from 'react';
-
+import { useState } from 'react';
 function Contact(props) {
+  
+    const[contact,SetContact]=useState(false)
+     const AddContact=()=>{
+        SetContact(true)
+     }
     return (
         <div>
         <div style={{height:"50px",backgroundColor:"rgb(11, 165, 204)",marginTop:"-20px"}}>
         <h1 style={{textAlign:"center"}}>Contact Page</h1>
         </div>
-            
+        {
+            contact===false?
+        
+            <div style={{textAlign:"center",marginTop:"20px"}}>
+                <button style={{width:"100px",height:"40px",fontSize:"20px"}} onClick={AddContact}>Contact</button> 
+                <div>
+                   <p>
+                   No Contact Found Please add contact from Create Contact Buttom
+                   </p>
+                </div>
+            </div>:
+            <div>
+            <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+               <div style={{margin:"auto",display:"block"}}>
+                 <div style={{display:"flex",justifyContent:"space-evenly"}}>
+                <h2>First Name :</h2>
+                <input type="text" style={{height:"40px",marginTop:"15px",marginLeft:"10px"}}/>
+              </div>
+              <div style={{display:"flex",justifyContent:"space-evenly"}}>
+                <h2>Last Name :</h2>
+                <input type="text" style={{height:"40px",marginTop:"20px",marginLeft:"10px"}}/>
+              </div>
+
+
+
+              <div style={{display:"flex",justifyContent:"space-evenly"}}>
+                <h1>Status:</h1>
+               
+               <div style={{marginTop:"20px",fontSize:"20px",fontWeight:"bold"}}>
+              <div>
+                <input
+                  type="radio"
+                />
+                Active
+              </div>
+              <div className="col-2">
+                <input
+                  type="radio"
+                />
+                Inactive
+              </div>
+              </div>
+               </div>
+
+            </div>
+           
+          </div>
+          <div style={{textAlign:"center",marginTop:"20px"}}>
+          <button style={{height:"40px",fontSize:"20px"}}>Save Contact</button> 
+            </div>
+          </div>
+        }
         </div>
     );
 }
--- taiyo/src/Components/Maps/Map.jsx
+++ taiyo/src/Components/Maps/Map.jsx
@@ -6,6 +6,11 @@ function Map(props) {
         <div style={{height:"50px",backgroundColor:" rgb(11, 165, 204)",marginTop:"-20px"}}>
         <h1 style={{textAlign:"center"}}>Map Page</h1>
         </div>
+          <div>
+           
+          
+          </div>
+
         </div>
     );
 }
