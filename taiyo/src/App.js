c7cf9373af68634d8a5725696e3165ace745ba3e
himangshumahato82 <himangshumahato82@gmail.com>
2023-05-11 17:10:44 +0530

    update

--- taiyo/src/App.js
+++ taiyo/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Route, Routes} from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Contact from "./Components/Contacts/Contact";
-import Map from "./Components/Maps/Map";
+import Dashboard from "./Components/Maps/Dashboard";
+const queryClient = new QueryClient();
 function App() {
+
   return (
     <div className="App">
+  
       <h1 style={{textAlign:"center"}}> Taiyo.AI Assignment</h1>
       <Routes>
       <Route path="/" element={<Home />}>
-      <Route path="map" element={<Map/>} />
+      
+  <Route path="map" element={<QueryClientProvider client={queryClient}>
+  <Dashboard />
+</QueryClientProvider>} />
       <Route path="contact" element={<Contact/>} />
        
        </Route>
